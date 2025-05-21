ea62fd832980dcbfb0b35b04edc45ca6fab40255
erikhenden <133907795+erikhenden@users.noreply.github.com>
2025-05-21 20:07:50 +0200

    Create quiz_game.py
    from mobile

---
+++ Stage_2/quiz_game.py
@@ -0,0 +1,12 @@
+"""🕹️ Game Project 2: Quiz Game
+Store questions/answers in a dictionary
+
+Randomly select questions
+
+Score tracking with results"""
+
+import random
+
+questions = {}
+
+score = 0
